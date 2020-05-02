c05a000ce65a09061521d9c015c281d9a9aa517d
Ankit0607 <40750212+Ankit0607@users.noreply.github.com>
2020-05-02 11:31:46 +0530

    Add files via upload

---
+++ Teacher Module/animation.php
@@ -0,0 +1,2 @@
+<input name="animation" type="hidden">
+   <div id="scrollToTop" class="scrollToTop mbr-arrow-up"><a style="text-align: center;"><i class="mbr-arrow-up-icon mbr-arrow-up-icon-cm cm-icon cm-icon-smallarrow-up"></i></a></div>
