35e73520241c518d479dfd0e3aafcda7f5184ffd
Ankit Mishra <40750212+Ankit0607@users.noreply.github.com>
2020-05-21 09:19:27 +0530

    Add files via upload
    New Teacher Module. Design will be same for hod and admin also but we will use previous design before login section

---
+++ Updated/nav2.php
@@ -0,0 +1,56 @@
+<nav class="navbar navbar-light navbar-expand bg-white shadow mb-4 topbar static-top">
+                    <div class="container-fluid"><button class="btn btn-link d-md-none rounded-circle mr-3" id="sidebarToggleTop" type="button"><i class="fas fa-bars"></i></button>
+                        <form class="form-inline d-none d-sm-inline-block mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
+                            <div class="input-group"><input class="bg-light form-control border-0 small" type="text" placeholder="Search for ...">
+                                <div class="input-group-append">
+                                    <button class="btn btn-primary py-0" type="button">
+                                        <i class="fas fa-search">
+                                        </i>
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+
+                        <ul class="nav navbar-nav flex-nowrap ml-auto">
+                            <li class="nav-item dropdown d-sm-none no-arrow">
+                                <a class="dropdown-toggle nav-link" data-toggle="dropdown" aria-expanded="false" href="#">
+                                    <i class="fas fa-search">
+                                    </i>
+                                </a>
+                                <div class="dropdown-menu dropdown-menu-right p-3 animated--grow-in" role="menu" aria-labelledby="searchDropdown">
+                                    <form class="form-inline mr-auto navbar-search w-100">
+                                        <div class="input-group">
+                                            <input class="bg-light form-control border-0 small" type="text" placeholder="Search for ...">
+                                            <div class="input-group-append">
+                                                <button class="btn btn-primary py-0" type="button">
+                                                    <i class="fas fa-search"> </i>
+                                                </button>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
+                            </li>
+                            
+                            <div class="d-none d-sm-block topbar-divider">
+                            </div>
+                            <li class="nav-item dropdown no-arrow" role="presentation">
+                                <div class="nav-item dropdown no-arrow">
+                                    <a class="dropdown-toggle nav-link" data-toggle="dropdown" aria-expanded="false" href="#">
+                                        <span class="d-none d-lg-inline mr-2 text-gray-600 small">Ankit@UIT</span>
+                                        <img class="border rounded-circle img-profile" src="assets/img/avatars/avatar1.jpeg">
+                                    </a>
+                                    <div class="dropdown-menu shadow dropdown-menu-right animated--grow-in" role="menu">
+                                        <a class="dropdown-item" role="presentation" href="profile.php">
+                                            <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400">
+                                            </i>&nbsp;Profile
+                                        </a>
+                                            
+                                            <a class="dropdown-item" role="presentation" href="#"><i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>&nbsp;Settings</a>
+                                        <a class="dropdown-item" role="presentation" href="#"><i class="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>&nbsp;Activity log</a>
+                                            <div class="dropdown-divider"></div><a class="dropdown-item" role="presentation" href="#"><i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>&nbsp;Logout</a>
+                                    </div>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
---
+++ Updated/show.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Result  Analyser</title>
+    <link rel="shortcut icon" href="../Teacher Module/assets/images/dlogo.png" type="image/x-icon">
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i">
+    <link rel="stylesheet" href="assets/fonts/fontawesome-all.min.css">
+
+
+
+</head>
+
+<body id="page-top">
+    <div id="wrapper">
+        <?php include "nav.php" ?>
+        <div class="d-flex flex-column" id="content-wrapper">
+            <div id="content">
+                <?php include "nav2.php" ?>
+                <div class="container">
+               
+                <div class="row">
+                    <div class="col-lg-12 col-xl-12">
+                        <div class="card shadow mb-4">
+                            <div class="card-header d-flex justify-content-between align-items-center">
+                                <h6 class="text-primary font-weight-bold m-0">Show Record</h6>
+                                
+                            </div>
+                            
+                            <table class="table table-borderless">
+                                <tr>
+                                    <td>
+                                        Year<select name="y1" class="form-control">
+                                            <option>1st Year</option>
+                                            <option>2nd Year</option>
+                                            <option>3rd Year</option>
+                                            <option>4th Year</option>
+                                        </select>
+                                    </td>
+
+                                    <td>
+                                        Branch<select name="bh1" class="form-control">
+                                            <option>Computer Science</option>
+                                            <option>Information Technology</option>
+                                            <option>Electrical Engineering</option>
+                                            <option>Electrical & Electronics</option>
+                                            <option>Mechanical Engineering</option>
+                                            <option>Civil Engineering</option>
+                                        s</select>
+                                    </td>
+
+                                    <td>
+                                        Semester<select name="s1" class="form-control">
+                                            <option>Odd</option>
+                                            <option>Even</option>
+                                        </select>
+                                    </td>
+
+                                    <td>
+                                        Subject<select name="s1" class="form-control">
+                                            <option>1</option>
+                                            <option>2</option>
+                                            <option>3</option>
+                                            <option>4</option>
+                                            <option>5</option>
+                                            <option>6</option>
+                                            <option>7</option>
+                                            <option>8</option>
+                                        </select>
+                                    </td>
+                                </tr>
+
+                                <tr>
+                                    <td>
+                                        <input type="submit" value="Search" name="b1" class="btn btn-primary"/><br>
+                                    </td>
+                                </tr>
+                            </table>
+                        </div>
+
+
+                        
+                    </div>
+
+
+                    </div>
+                    
+            </div>
+           
+        </div>
+    </div>
+    </div>
+    
+    <a class="border rounded d-inline scroll-to-top" href="#page-top"><i class="fas fa-angle-up"></i></a></div>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="assets/js/chart.min.js"></script>
+    <script src="assets/js/bs-init.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.js"></script>
+    <script src="assets/js/theme.js"></script>
+
+
+</html>
