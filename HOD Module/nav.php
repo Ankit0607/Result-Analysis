831e923809bd2785d8a796d26336884c48793ba9
Ankit Mishra <40750212+Ankit0607@users.noreply.github.com>
2020-06-03 08:46:53 +0530

    Add files via upload
    HOD Module complete frontend backend

---
+++ HOD Module/nav.php
@@ -0,0 +1,29 @@
+<nav class="navbar navbar-dark align-items-start sidebar sidebar-dark accordion bg-gradient-primary p-0">
+            <div class="container-fluid d-flex flex-column p-0">
+            <span class="navbar-logo">
+                <a href="profile.php">
+                     <img src="assets/img/dlogo.png" alt="Mobirise" style="height: 3.8rem;">
+                </a>
+            </span>
+                <a class="navbar-brand d-flex justify-content-center align-items-center sidebar-brand m-0" href="#">
+                    <!-- <div class="sidebar-brand-icon rotate-n-15"><i class="fas fa-laugh-wink"></i></div><br> -->
+                    
+                    <div class="sidebar-brand-text mx-3"><span>Result analyser&nbsp;</span></div>
+                </a>
+                <hr class="sidebar-divider my-0">
+                <ul class="nav navbar-nav text-light" id="accordionSidebar">
+                    <li class="nav-item" role="presentation"><a class="nav-link active" href="index.php"><i class="fas fa-tachometer-alt"></i><span>Dashboard</span></a></li>
+                    <li class="nav-item" role="presentation"><a class="nav-link" href="#"><i class="fas fa-user"></i><span>Department Record</span></a></li>
+                    <li class="nav-item" role="presentation"><a class="nav-link" href="teacher_record.php"><i class="fas fa-table"></i><span>Teacher Record</span></a></li>
+                    <li class="nav-item" role="presentation"><a class="nav-link" href="analysis.php"><i class="far fa-user-circle"></i><span>&nbsp;New Registration</span></a></li>
+                    <li class="nav-item" role="presentation"><a class="nav-link" href="manage_subject.php"><i class="fas fa-user-circle"></i><span>Manage Subjects</span></a></li>
+                    <li class="nav-item" role="presentation"><a class="nav-link" href="dept_analysis.php"><i class="fas fa-user-circle"></i><span>Deaprtment Analysis</span></a></li>
+                    <li class="nav-item" role="presentation"><a class="nav-link" href="student_analysis.php"><i class="fas fa-user-circle"></i><span>Student Analysis</span></a></li>
+                    <li class="nav-item" role="presentation"><a class="nav-link" href="teacher_analysis.php"><i class="fas fa-user-circle"></i><span>Teacher Analysis</span></a></li>
+
+
+                    <li class="nav-item" role="presentation"><a class="nav-link" href="../Teacher Module/developer.php"><i class="fas fa-table"></i><span>Developers</span></a></li>
+                </ul>
+                <div class="text-center d-none d-md-inline"><button class="btn rounded-circle border-0" id="sidebarToggle" type="button"></button></div>
+            </div>
+        </nav>
